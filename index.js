e3d3bde5e36f31a339183a6962564650c3ce15e2
ala1ahmed <38598250+ala1ahmed@users.noreply.github.com>
2020-12-22 15:58:23 +0100

    init frontend + install axios

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use('/api',apiRouter);
 
 app.listen(port, () => `Server running on port ${port} 🔥`);
